6d2662e2eaa3bf77d2a48a6cbedbb13fb425d63f
yuu <muhdamirul.abdaziz@gmail.com>
2019-03-17 08:32:52 +0800

    update

--- routes/comments.js
+++ routes/comments.js
@@ -35,11 +35,62 @@ router.post('/', isLoggedIn, function(req, res) {
   });
 });
 
+router.get('/:comment_id/edit', checkCommentOwnership, function(req, res) {
+  Comment.findById(req.params.comment_id, function(err, foundComment) {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.render('comments/edit', {
+        campground_id: req.params.id,
+        comment: foundComment
+      });
+    }
+  });
+});
+
+router.put('/:comment_id/', checkCommentOwnership, function(req, res) {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(
+    err,
+    updatedComment
+  ) {
+    if (err) {
+      res.redirect('back');
+    } else {
+      console.log(updatedComment);
+      res.redirect('/campgrounds/' + req.params.id);
+    }
+  });
+});
+
+router.delete('/:comment_id', checkCommentOwnership, function(req, res) {
+  Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+    if (err) {
+      res.redirect('back');
+    } else {
+      res.redirect('back');
+    }
+  });
+});
+
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect('/login');
+}
+
+function checkCommentOwnership(req, res, next) {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+      if (foundComment.author.id.equals(req.user._id)) {
+        return next();
+      } else {
+        res.redirect('back');
+      }
+    });
+  } else {
+    res.redirect('/login');
+  }
 }
 
 module.exports = router;
